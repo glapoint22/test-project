7167701b1929f1c0564baabdfc572dbe5ebe0497
glapoint22 <glapoint22@gmail.com>
2024-07-26 16:43:06 -0400

    Working on radio button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, Renderer2 } from '@angular/core';
 import { IconComponent, RadioButtonComponent, CheckboxComponent } from 'Components';
 
 @Component({
@@ -9,5 +9,27 @@ import { IconComponent, RadioButtonComponent, CheckboxComponent } from 'Componen
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  protected buttonLabel!: string;
+  private renderer = inject(Renderer2);
+
+
+  ngOnInit() {
+    this.renderer.addClass(document.body, 'light-theme');
+    this.buttonLabel = 'Dark Theme';
+  }
+
+
   
+  
+  protected onClick(): void {
+    if (this.buttonLabel === 'Dark Theme') {
+      this.buttonLabel = 'Light Theme';
+      this.renderer.addClass(document.body, 'dark-theme');
+      this.renderer.removeClass(document.body, 'light-theme');
+    } else {
+      this.buttonLabel = 'Dark Theme';
+      this.renderer.addClass(document.body, 'light-theme');
+      this.renderer.removeClass(document.body, 'dark-theme');
+    }
+  }
 }
